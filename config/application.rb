ae5402ea01d02a4c358b462b55c3dd70cfe62d75
moeka-okubo <moeka.okubo@arsaga.jp>
2025-07-08 17:43:05 +0900

    コンポーネントを使って書き換えた

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module TodoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("app/components")
   end
 end
