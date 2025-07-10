6d47e65ea86e554168328c1e0ef4c061e144bdd8
moeka-okubo <moeka.okubo@arsaga.jp>
2025-07-10 13:33:53 +0900

    バリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :title,
+    presence: { message: "タイトルを入力してください" },
+    length: { minimum: 3, message: "タイトルは3文字以上で入力してください" }
+  validates :description, length: { maximum: 100, message: "内容は100文字以下で入力してください" }
 end
